4d20514e6791dc894daac268b35281d315a480d7
Artem Butbaev <Artem.Butbaev@PACONSULTING.COM>
2022-12-05 13:13:57 +0000

    updated day 4 code to use range() function

--- day_4/day4.py
+++ day_4/day4.py
@@ -1,25 +1,20 @@
+import time
 from utils import *
 
 def convert_shorthand_to_list_of_sections(sections_shorthand: str) -> list:
-
+    
     dash_index = sections_shorthand.index('-')
-    first_number, second_number = sections_shorthand[:dash_index], sections_shorthand[dash_index+1:]
-    numbers_to_add = int(second_number) - int(first_number)
-    
-    numbers_list = [int(first_number), int(second_number)]
+    first_number, second_number = int(sections_shorthand[:dash_index]), int(sections_shorthand[dash_index+1:])
 
-    to_add = int(first_number) + 1
-    for _ in range(0, numbers_to_add - 1):
-        numbers_list.append(to_add)
-        to_add += 1
-    
-    return numbers_list
+    return list(range(first_number, second_number + 1))
 
 def main():
     # import data
     cleaning_sections_data = read_data("day_4/data/data.txt")
-    overlaps = 0
 
+    start_time = time.time()
+    
+    overlaps = 0
     for pair_section_assignment in cleaning_sections_data:
 
         # 1. split string into 2 using split(,) using comma as delimiter
@@ -27,8 +22,8 @@ def main():
         
         # 2. convert each assignment (e.g., 2-4) into a list of numbers, [2,3,4]
         elf_1_sections, elf_2_sections = convert_shorthand_to_list_of_sections(elf_1_sections), convert_shorthand_to_list_of_sections(elf_2_sections)
-        
-        # 3. identify overlap of two listss, e.g., [2,3,4] and [6,7,8] (using intersection method)
+
+        # 3. identify overlap of two lists, e.g., [2,3,4] and [6,7,8] (using intersection method)
         intersection = get_intersection_of_two_items(elf_1_sections, elf_2_sections)
         
         # 4. check for overlaps
@@ -42,6 +37,7 @@ def main():
             overlaps +=1
 
     print(overlaps)
+    print("--- %s seconds ---" % (time.time() - start_time))
 
 if __name__ == "__main__":
     main()
