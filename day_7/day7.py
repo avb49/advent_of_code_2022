a479336a3502f6f83c32491c649d411db97e4409
Artem Butbaev <abutbaev@gmail.com>
2022-12-08 21:13:17 +0000

    Day 7 completed

--- day_7/day7.py
+++ day_7/day7.py
@@ -3,20 +3,12 @@ import re
 
 from utils import *
 
-class File:
-
-    def __init__(self, name, size, dir):
-        self.name = name
-        self.size = size
-        self.dir = dir
-
 class Directory:
 
-    def __init__(self, name, size, parent_dir, children=[]):
+    def __init__(self, name, size, parent_dir):
         self.name = name
         self.size = size
         self.parent_dir = parent_dir
-        self.children = children
 
 # finds name of directory (1 group)
 COMMAND_SEARCH_PATTERN = r"cd (.*)"
@@ -27,60 +19,82 @@ FILE_SEARCH_PATTERN = r"([0-9]*) (.*)"
 # finds name of a directory
 DIR_SEARCH_PATTERN = r"dir (.*)"
 
+MAX_DIRECTORY_SIZE = 100000
+TOTAL_DISK_SPACE = 70000000
+DISK_SPACE_NEEDED_FOR_UPDATE = 30000000
+
+# Part 1 - returns the total size of directories smaller than max. directory size
+def get_sum_of_directories_within_requirements(directories: list) -> int:
+
+    directories_within_requirements = []
+    for dir in directories:
+
+        if dir.size <= MAX_DIRECTORY_SIZE:
+            directories_within_requirements.append(dir)
+        
+    total_size = 0
+    for dir in directories_within_requirements:
+        total_size += dir.size
+    
+    return total_size
+
+# Part 2 - returns the smallest directory size, which, upon deletion allows for installing the update
+def get_best_directory_to_delete(directories: list, root_directory: Directory) -> int:
+
+    unused_space = TOTAL_DISK_SPACE - root_directory.size
+    to_be_deleted = DISK_SPACE_NEEDED_FOR_UPDATE - unused_space
+    directory_size = DISK_SPACE_NEEDED_FOR_UPDATE
+
+    for dir in directories:
+        if (dir.size < directory_size) & (dir.size >= to_be_deleted):
+            directory_size = dir.size
+    
+    return directory_size
+
 def main():
 
-    # 1. import data
-    filesystem_data = read_data("day_7/data/test_data.txt")
+    # import data
+    filesystem_data = read_data("day_7/data/data.txt")
 
     start_time = time.time()
+    root_directory = Directory("/", 0, None)
+    current_dir = root_directory 
 
-    root_dir = Directory("/", None, None, None)
-    current_dir = root_dir
+    directories = []
 
-    # 2. read line and determine if command or directory/file
     for line_index in range(0, len(filesystem_data)):
 
-        #TODO: handle when '..' is used - update current directory
         if "$ cd .." == filesystem_data[line_index]:
             current_dir = current_dir.parent_dir
-            print(f"Going up a directory... From {current_dir.name} to {current_dir.parent_dir.name}")
 
         elif "$ cd" in filesystem_data[line_index]:
-            # create new Directory object
+            # create new Directory object and add to list of directories
             directory_name = re.search(COMMAND_SEARCH_PATTERN, filesystem_data[line_index]).group(1)
-            directory = Directory(directory_name, None, current_dir, None)
+            directory = Directory(directory_name, 0, current_dir)
+            directories.append(directory)
             current_dir = directory
-            print(f"cd'ing into new directory... Current directory: {current_dir.name}. Parent dir is {current_dir.parent_dir.name}")
 
         elif filesystem_data[line_index][0] != "$":
 
-            # for the current_dir, add children
+            # match line to file regex search pattern
             file_match_object = re.search(FILE_SEARCH_PATTERN, filesystem_data[line_index])
-            if file_match_object.group(1) == '':
-                dir_match_object = re.search(DIR_SEARCH_PATTERN, filesystem_data[line_index])
-                print(f"Directory discovered: {dir_match_object.group(1)}")
 
-            else:
-                file_size, file_name = file_match_object.group(1), file_match_object.group(2)
-                print(f"File discovered with filename: {file_name} and file size: {file_size}")
+            # if there is a match, then it is a file
+            if file_match_object.group(1) != '':
+                # get file size
+                file_size = file_match_object.group(1)
+                # add file size to current directory and all its parents
+                to_update = current_dir
+                while(to_update is not None):
+                    to_update.size += int(file_size)
+                    to_update = to_update.parent_dir
 
+    # part 1 answer
+    print(get_sum_of_directories_within_requirements(directories))
+    # part 2 answer
+    print(get_best_directory_to_delete(directories, root_directory))
 
-        elif "$ ls" in filesystem_data[line_index]:
-            pass
-                
-
-    # 3. construct file tree using commands and info:
-    # - cd: add a node if doesnt exit, otherwise keep track of current node
-    # - ls: add children nodes to nodes 
-
-    #file1 = File(1000, "/")
-    #directory1 = Directory(None, "/", [file1])
-    #for file in directory1.children:
-    #    print(file)
-
-    print()
     print("--- %s seconds ---" % (time.time() - start_time))
-
 
 if __name__ == "__main__":
     main()
